af964dc097d6215f4177a69c4815e890408bb7fe
C. Roldán Piñero <roldanpineroc@gmail.com>
2025-08-06 14:31:53 +0200

    reformat

--- lua/options.lua
+++ lua/options.lua
@@ -1,44 +1,44 @@
--- See `:help lua-options` and `:help lua-options-guide`
+local set = vim.opt
 
 -- Make line numbers default
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.signcolumn = 'yes'
+set.number = true
+set.relativenumber = true
+set.signcolumn = 'yes'
 
 -- No wrap at the end of the screen
-vim.o.wrap = false
+set.wrap = false
 
 -- Spaces, no tabs and default indent without LSP
-vim.o.expandtab = true
-vim.o.shiftwidth = 2
+set.expandtab = true
+set.shiftwidth = 2
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+set.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
+set.showmode = false
 
 -- Save undo history
-vim.o.undofile = true
+set.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
+set.ignorecase = true
+set.smartcase = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+set.list = true
+set.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
+set.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.o.cursorline = true
+set.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 8
+set.scrolloff = 8
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.o.confirm = true
+set.confirm = true
