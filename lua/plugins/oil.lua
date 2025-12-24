bac1aed0719d8ac1ac146d84aee464b3fc412c69
C. Roldán Piñero <roldanpineroc@disroot.org>
2025-12-24 13:23:40 +0100

    hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,7 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   lazy = false,
   config = function()
-    require('oil').setup()
+    require('oil').setup { view_options = { show_hidden = true } }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
 }
